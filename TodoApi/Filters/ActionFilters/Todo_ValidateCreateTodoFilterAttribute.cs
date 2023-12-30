ee93ba6378d846bc810ab0f7ab52a417711f26bc
hav0kinho <ruallysonfelype@gmail.com>
2023-12-30 07:20:29 -0300

    Criação dos Action Filters. Um para validação de ID passado, e outro para validação de criação de Todos

---
+++ TodoApi/Filters/ActionFilters/Todo_ValidateCreateTodoFilterAttribute.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using TodoApi.Data;
+using TodoApi.Models;
+
+namespace TodoApi.Filters.ActionFilters
+{
+    public class Todo_ValidateCreateTodoFilterAttribute : ActionFilterAttribute
+    {
+        private readonly AppDbContext _db;
+        public Todo_ValidateCreateTodoFilterAttribute(AppDbContext db)
+        {
+            _db = db;
+        }
+
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            base.OnActionExecuting(context);
+
+            var todo = context.ActionArguments["todo"] as Todo;
+
+            if(todo == null) // Se Todo não foi passado na requisição
+            {
+                context.ModelState.AddModelError("Todo", "Todo não pode ser nulo, deve ser enviado.");
+                var problemDetails = new ValidationProblemDetails(context.ModelState)
+                {
+                    Status = StatusCodes.Status400BadRequest
+                };
+                context.Result = new BadRequestObjectResult(problemDetails);
+            }
+            else // Todo foi passado, então verifica se já existe um Todo igual cadastrado (mesma tarefa e estado).
+            {
+                var existingTodo = _db.Todos.FirstOrDefault(
+                    x => x.Title!.ToLower() == todo.Title!.ToLower() &&
+                         x.IsCompleted == todo.IsCompleted
+                );
+
+                if (existingTodo != null) // Encontrou um todo igual? Então manda o erro e um BadRequest
+                {
+                    context.ModelState.AddModelError("Todo", "Todo com o mesmo título e estado já existe.");
+                    var problemDetails = new ValidationProblemDetails(context.ModelState)
+                    {
+                        Status = StatusCodes.Status400BadRequest
+                    };
+                    context.Result = new BadRequestObjectResult(problemDetails);
+                }
+            }
+        }
+    }
+}
---
+++ TodoApi/Filters/ActionFilters/Todo_ValidateTodoIdFilterAttribute.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using TodoApi.Data;
+
+namespace TodoApi.Filters.ActionFilters
+{
+    public class Todo_ValidateTodoIdFilterAttribute : ActionFilterAttribute
+    {
+        private readonly AppDbContext _db;
+
+        public Todo_ValidateTodoIdFilterAttribute(AppDbContext db)
+        {
+            _db = db;
+        }
+
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            base.OnActionExecuting(context);
+
+            var todoId = context.ActionArguments["id"] as int?;
+            if(todoId.HasValue) // Id foi passado
+            {
+                if(todoId.Value <= 0) // Id é menor que 0? Manda o problema da requisião e um BadRequest
+                {
+                    context.ModelState.AddModelError("TodoId", "TodoId é inválido, pois é menor que zero.");
+                    var problemDetails = new ValidationProblemDetails(context.ModelState)
+                    {
+                        Status = StatusCodes.Status400BadRequest
+                    };
+                    context.Result = new BadRequestObjectResult(problemDetails);
+                }
+                else // O id é maior ou igual que zero
+                {
+                    var todo = _db.Todos.Find(todoId);
+                    if(todo == null) // Não encontrou o Todo? Manda o problema na requisição e um NotFound 
+                    {
+                        context.ModelState.AddModelError("TodoId", "O Todo não existe.");
+                        var problemDetails = new ValidationProblemDetails(context.ModelState)
+                        {
+                            Status = StatusCodes.Status404NotFound
+                        };
+                        context.Result = new NotFoundObjectResult(problemDetails);
+                    }
+                    else // O Todo existe, manda pro controller via HttpContext
+                    {
+                        context.HttpContext.Items["todo"] = todo;
+                    }
+                }
+            }
+        }
+    }
+}
